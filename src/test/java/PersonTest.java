53a862a140c5dd5ede7e9410e28dccde25262a41
Tobias Hedkvist <tobias.hedkvist@hotmail.com>
2017-11-21 11:14:07 +0100

    added empty test cases for classes student and classroom, plus one function in test case persontest

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -7,9 +7,12 @@ import otherclasses.Person;
 public class PersonTest {
 
 	@Test
-	public void test() {
+	public void testPerson() {
 		Person p = new Person("Rafael", "Silva", 30,'M');
 		assertEquals(p.getFirstName().equals("Rafael"),true);
+		assertEquals(p.getLastName().equals("Silva"),true);
+		assertEquals(p.getAge(),30, 0);
+		assertEquals(p.getGender(),'M', 0);
 	}
 
 }
