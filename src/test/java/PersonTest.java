6d70e43cfe350f310144d3628b3fc31f7721f0fa
Tobias Hedkvist <tobias.hedkvist@hotmail.com>
2017-11-21 11:55:49 +0100

    added all test cases in PertonTest

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -5,14 +5,43 @@ import org.junit.Test;
 import otherclasses.Person;
 
 public class PersonTest {
-
+	Person p = new Person("Rafael", "Silva", 30,'M');
 	@Test
 	public void testPerson() {
-		Person p = new Person("Rafael", "Silva", 30,'M');
+		
 		assertEquals(p.getFirstName().equals("Rafael"),true);
 		assertEquals(p.getLastName().equals("Silva"),true);
 		assertEquals(p.getAge(),30, 0);
 		assertEquals(p.getGender(),'M', 0);
 	}
+	@Test
+	public void testSetFirstName() {
+		p.setFirstName("Rafael");
+		String firstName = p.getFirstName();
+		assertTrue(firstName.equals("Rafael"));
+		
+	}
+	@Test
+	public void testSetLastName() {
+		p.setLastName("Silva");
+		String lastName = p.getLastName();
+		assertTrue(lastName.equals("Silva"));
+		
+	}
+	@Test
+	public void testSetAge() {
+		p.setAge(30);;
+		int age = p.getAge();
+		assertTrue(age == 30);
+		
+	}
+	@Test
+	public void testSetGender() {
+		p.setGender('M');
+		char gender = p.getGender();
+		assertTrue(gender == 'M');
+		
+	}
+
 
 }
